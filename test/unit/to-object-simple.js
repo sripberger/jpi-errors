e765649753175946c5a0205d9f17b5b35bbe420b
Steve Ripberger <sripberger@batterii.com>
2019-02-24 00:40:02 -0500

    Improve toObjectSimple

--- test/unit/to-object-simple.js
+++ test/unit/to-object-simple.js
@@ -1,4 +1,5 @@
 import * as nani from 'nani';
+import { JpiError } from '../../lib/jpi-error';
 import { toObjectSimple } from '../../lib/to-object-simple';
 
 describe('toObjectSimple', function() {
@@ -10,6 +11,7 @@ describe('toObjectSimple', function() {
 		err = new Error(message);
 		err.name = name;
 
+		sinon.stub(nani, 'is').returns(false);
 		sinon.stub(nani, 'getFullName').returns(null);
 	});
 
@@ -20,22 +22,48 @@ describe('toObjectSimple', function() {
 		});
 	});
 
-	it('includes code at top level, if it is an integer', function() {
-		const code = err.code = 0;
+	context('err is a JpiError', function() {
+		beforeEach(function() {
+			nani.is.withArgs(JpiError, err).returns(true);
+		});
 
-		expect(toObjectSimple(err)).to.deep.equal({
-			message,
-			code,
-			data: { name },
+		it('includes code at top level if it is an integer', function() {
+			const code = err.code = 42;
+
+			expect(toObjectSimple(err)).to.deep.equal({
+				message,
+				code,
+				data: { name },
+			});
+		});
+
+		it('ignores code if it is not an integer', function() {
+			err.code = 3.14;
+
+			expect(toObjectSimple(err)).to.deep.equal({
+				message,
+				data: { name },
+			});
 		});
 	});
 
-	it('includes code in data, if it is not an integer', function() {
-		const code = err.code = 3.14;
+	context('err is not a JpiError', function() {
+		it('includes code in data, if any', function() {
+			const code = err.code = 'error_code';
 
-		expect(toObjectSimple(err)).to.deep.equal({
-			message,
-			data: { name, code },
+			expect(toObjectSimple(err)).to.deep.equal({
+				message,
+				data: { name, code },
+			});
+		});
+
+		it('supports undefined code', function() {
+			err.code = undefined;
+
+			expect(toObjectSimple(err)).to.deep.equal({
+				message,
+				data: { name, code: undefined },
+			});
 		});
 	});
 
@@ -58,6 +86,15 @@ describe('toObjectSimple', function() {
 		});
 	});
 
+	it('supports undefined shortMessage', function() {
+		err.shortMessage = undefined;
+
+		expect(toObjectSimple(err)).to.deep.equal({
+			message,
+			data: { name, shortMessage: undefined },
+		});
+	});
+
 	it('includes error info in data, if any', function() {
 		const info = err.info = { foo: 'bar' };
 
@@ -67,7 +104,16 @@ describe('toObjectSimple', function() {
 		});
 	});
 
-	it('includes stack, if includeStack argument is true', function() {
+	it('supports undefined info', function() {
+		err.info = undefined;
+
+		expect(toObjectSimple(err)).to.deep.equal({
+			message,
+			data: { name, info: undefined },
+		});
+	});
+
+	it('includes stack, if specified', function() {
 		expect(toObjectSimple(err, true)).to.deep.equal({
 			message,
 			data: { name, stack: err.stack },
