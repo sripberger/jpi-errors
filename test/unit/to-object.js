5e65de0f17c4eadaf27222c423828bf332df7e9e
Steve Ripberger <sripberger@batterii.com>
2019-02-23 21:25:40 -0500

    Implement external toObject function

--- test/unit/to-object.js
+++ test/unit/to-object.js
@@ -1,3 +1,20 @@
+import * as converterModule from '../../lib/converter';
+import { toObject } from '../../lib/to-object';
+
 describe('toObject', function() {
-	// TODO
+	it('converts error to an object using a new Converter instance', function() {
+		const err = new Error('Omg bad error!');
+		const obj = { foo: 'bar' };
+		const converter = sinon.createStubInstance(converterModule.Converter);
+		converter.convert.returns(obj);
+		sinon.stub(converterModule, 'Converter').returns(converter);
+
+		const result = toObject(err);
+
+		expect(converterModule.Converter).to.be.calledOnce;
+		expect(converterModule.Converter).to.be.calledWithNew;
+		expect(converter.convert).to.be.calledOnce;
+		expect(converter.convert).to.be.calledWith(err);
+		expect(result).to.equal(obj);
+	});
 });
