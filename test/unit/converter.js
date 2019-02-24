bcf5db5f5cdb1b825670bb01c554015a6152f1b7
Steve Ripberger <sripberger@batterii.com>
2019-02-23 20:43:30 -0500

    Add internal Converter class

---
+++ test/unit/converter.js
@@ -0,0 +1,130 @@
+import * as nani from 'nani';
+import * as toObjectSimpleModule from '../../lib/to-object-simple';
+import { Converter } from '../../lib/converter';
+
+describe('Converter', function() {
+	let converter;
+
+	beforeEach(function() {
+		converter = new Converter();
+	});
+
+	it('creates an empty map on objectsByError', function() {
+		expect(converter.objectsByError).to.be.an.instanceof(Map);
+		expect(converter.objectsByError).to.be.empty;
+	});
+
+	describe('#add', function() {
+		let err, obj, toObjectSimple;
+
+		beforeEach(function() {
+			err = new Error('Omg bad error!');
+			obj = { message: err.message, data: {} };
+
+			toObjectSimple = sinon.stub(toObjectSimpleModule, 'toObjectSimple')
+				.withArgs(err).returns(obj);
+		});
+
+		it('converts err to obj using toObjectSimple', function() {
+			converter.add({ err, parent: null, inArray: false });
+
+			expect(toObjectSimple).to.be.calledOnce;
+			expect(toObjectSimple).to.be.calledWith(err);
+		});
+
+		it('stores obj in objectsByError', function() {
+			converter.add({ err, parent: null, inArray: false });
+
+			expect(converter.objectsByError).to.have.keys([ err ]);
+			expect(converter.objectsByError.get(err)).to.equal(obj);
+		});
+
+		it('handles a single cause relationship', function() {
+			const parent = new Error('Parent error');
+			const parentObj = { data: {} };
+			converter.objectsByError.set(parent, parentObj);
+
+			converter.add({ err, parent, inArray: false });
+
+			expect(parentObj.data.cause).to.equal(obj);
+			expect(parentObj.data).to.not.have.property('errors');
+		});
+
+		it('handles the first cause in a multi-cause relationship', function() {
+			const parent = new Error('Parent error');
+			const parentObj = { data: {} };
+			converter.objectsByError.set(parent, parentObj);
+
+			converter.add({ err, parent, inArray: true });
+
+			expect(parentObj.data.cause).to.equal(obj);
+			expect(parentObj.data.errors).to.deep.equal([ obj ]);
+		});
+
+		it('handles subsequent causes in a multi-cause relationship', function() {
+			const cause = { foo: 'bar' };
+			const parent = new Error('Parent error');
+			const parentObj = { data: { cause, errors: [ cause ] } };
+			converter.objectsByError.set(parent, parentObj);
+
+			converter.add({ err, parent, inArray: true });
+
+			expect(parentObj.data.cause).to.equal(cause);
+			expect(parentObj.data.errors).to.deep.equal([ cause, obj ]);
+		});
+
+		it('does nothing if parent is not in objectsByError', function() {
+			const parent = new Error('Parent error');
+
+			converter.add({ err, parent, inArray: false });
+
+			expect(toObjectSimple).to.not.be.called;
+			expect(converter.objectsByError).to.be.empty;
+		});
+
+		it('does nothing if cause alread set and inArray is false', function() {
+			const cause = { foo: 'bar' };
+			const parent = new Error('Parent error');
+			const parentObj = { data: { cause } };
+			converter.objectsByError.set(parent, parentObj);
+
+			converter.add({ err, parent, inArray: false });
+
+			expect(toObjectSimple).to.not.be.called;
+			expect(parentObj.data.cause).to.equal(cause);
+			expect(converter.objectsByError).to.have.keys([ parent ]);
+		});
+	});
+
+	describe('#convert', function() {
+		let err, items;
+
+		beforeEach(function() {
+			err = new Error('Omg bad error!');
+			items = [ { foo: 'bar' }, { baz: 'qux' } ];
+
+			sinon.stub(nani, 'iterateFull').returns(items[Symbol.iterator]());
+			sinon.stub(converter, 'add');
+		});
+
+		it('adds all items from nani.iterateFull', function() {
+			converter.convert(err);
+
+			expect(nani.iterateFull).to.be.calledOnce;
+			expect(nani.iterateFull).to.be.calledWith(err);
+			expect(converter.add).to.be.calledTwice;
+			expect(converter.add.args).to.deep.equal(items.map((i) => [ i ]));
+		});
+
+		it('returns obj for err from objectsByError', function() {
+			const obj = { message: err.message, data: { name: err.name } };
+			converter.add.onSecondCall().callsFake(() => {
+				// Put the error in the map *after* iteration is done,
+				// to make sure we're not getting it too early.
+				converter.objectsByError.set(err, obj);
+			});
+
+			expect(converter.convert(err)).to.equal(obj);
+		});
+	});
+});
